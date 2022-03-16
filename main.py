00ca7370419d0a15fb3e13da7ad36b03fa6d910c
Pavel Shitikov <pashicop@gmail.com>
2022-03-16 19:06:26 +0300

    Task 3 Complete

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def create_list_files():
+    list = ['1.txt', '2.txt', '3.txt']
+    return list
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    list_files = create_list_files()
+    # print(list_files)
+    quantity = []
+    for file in list_files:
+        with open(file, encoding='utf-8') as f:
+            quantity.append(f.read().count('\n') + 1)
+    # print(quantity)
+    new_list = list(zip(list_files, quantity))
+    # print(new_list)
+    new_list = sorted(new_list, key=lambda x: x[1])
+    # print(new_list)
+    f = open("result.txt", "w")
+    f.close()
+    for f in new_list:
+        with open(f[0], encoding='utf-8') as old_file:
+            with open("result.txt", 'a') as new_file:
+                new_file.write(f[0] + '\n')
+                new_file.write(str(f[1])+'\n')
+                new_file.write(old_file.read() + '\n')
